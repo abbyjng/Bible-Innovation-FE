21d5966642040e435e6cc375d2cc0e38b67c4088
Abby Ng <abigailjng@gmail.com>
2023-07-13 07:16:08 -0400

    Add content to signout page

---
+++ pages/signout.tsx
@@ -0,0 +1,13 @@
+/* Sign out page */
+
+import MenuBarTop from "@/components/MenuBarTop";
+import { Page } from "@/utils/types";
+
+export default function SignOut() {
+  return (
+    <div className="">
+      <MenuBarTop currentPage={Page.PROFILE} />
+      <div className="p-2 pb-20">TODO</div>
+    </div>
+  );
+}
